bc9a99b4931ce3c1b7939f3d49863d62a26e7e55
pjt3591oo <pjt3591oo@gmail.com>
2022-11-13 11:00:15 +0900

    경로를 path를 이용하여 생성한다

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Post } from 'src/entity/post.entity';
-import { User } from 'src/entity/user.entity';
-import { DataSource } from 'typeorm';
+import { join } from 'path';
+
 import { databaseProviders } from './database.providers';
 
 @Module({
@@ -19,7 +18,7 @@ import { databaseProviders } from './database.providers';
           password: configService.get('database.password'),
           database: configService.get('database.database'),
           entities: [
-            __dirname + '/../**/*.entity{.ts,.js}',
+            join(__dirname, '/../**/*.entity{.ts,.js}'),
           ],
           synchronize: configService.get('database.synchronize'),
           logging: configService.get('database.logging'),
